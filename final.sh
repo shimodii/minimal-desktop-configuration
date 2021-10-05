9034356e89d44961215ac5b45a858a94f31aded8
Amir Mohammad <53581892+am-shm@users.noreply.github.com>
2021-10-05 17:19:13 +0330

    Update final.sh

--- final.sh
+++ final.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 ### make dir at ~/.config for configs ###
+mkdir ~/.config
 mkdir ~/.config/i3
 mkdir ~/.config/rofi
 mkdir ~/.config/lemonbar
+mkdir ~/.config/picom
 
 ### copy configs to dirs we made ###
 cp config ~/.config/i3/
 cp minimal.rasi ~/.config/rofi
 cp bar.sh ~/.config/lemonbar.sh
+cp picom.conf ~/.config/picom
